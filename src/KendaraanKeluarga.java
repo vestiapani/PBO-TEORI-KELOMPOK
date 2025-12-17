8e9e58f465ed94554650a17742046f73c755ed3f
vestiapani <ivan131335@gmail.com>
2025-12-17 15:16:10 +0700

    Tubes PBO Teori

--- src/KendaraanKeluarga.java
+++ src/KendaraanKeluarga.java
@@ -1,3 +1,29 @@
-public class KendaraanKeluarga {
-    
+public class KendaraanKeluarga extends KendaraanRoda4 {
+
+    private int pJumlahPintu;
+    private boolean pACBelakang;
+
+    public KendaraanKeluarga(String nama, String jenis, int kecepatan, int kapasitas,
+            String warna, String bahan, float harga, boolean status, String plat, int tahun,
+            int pintu, boolean acBelakang) {
+
+        super(nama, jenis, kecepatan, kapasitas, warna, bahan, harga, status, plat, tahun);
+        this.pJumlahPintu = pintu;
+        this.pACBelakang = acBelakang;
+    }
+
+    @Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Jumlah Pintu: " + pJumlahPintu);
+        System.out.println("AC Belakang: " + (pACBelakang ? "Ada" : "Tidak Ada"));
+    }
+
+    public void hidupkanACBelakang() {
+        System.out.println("AC belakang dinyalakan.");
+    }
+
+    public void lipatKursiBelakang() {
+        System.out.println("Kursi belakang dilipat.");
+    }
 }
--- src/KendaraanRoda2.java
+++ src/KendaraanRoda2.java
@@ -1,16 +1,39 @@
 public class KendaraanRoda2 extends Kendaraan {
 
+    protected boolean pKickStarter;
+
     public KendaraanRoda2(String nama, String jenis, int kecepatan, int kapasitas,
-            String warna, String bahan, float harga, boolean status, String plat, int tahun) {
+            String warna, String bahan, double harga, boolean status, String plat, int tahun,
+            boolean kickStarter) {
 
         super(nama, jenis, kecepatan, kapasitas, warna, bahan, harga, status, plat, tahun);
+        this.pKickStarter = kickStarter;
+    }
+
+    @Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Kick Starter: " + (pKickStarter ? "Ada" : "Tidak Ada"));
     }
 
     public void cekTekananBan() {
-        System.out.println("Tekanan ban kendaraan roda 2 sudah dicek dan baik.");
+        System.out.println("Tekanan ban " + pNama + " dicek: Aman untuk manuver miring.");
     }
 
     public void serviceRutin() {
-        System.out.println("Service kendaraan roda 2 telah dilakukan.");
+        System.out.println("Service rutin (Ganti Oli & Cek Rantai/CVT) selesai.");
+    }
+
+    public void turunkanStandar() {
+        System.out.println("Standar diturunkan. Motor parkir dengan seimbang.");
+    }
+
+    public void selapSelip() {
+        System.out.println(pNama + " bermanuver 'selap-selip' di antara kemacetan mobil.");
+        System.out.println(" berhasil melewati antrian panjang!");
+    }
+
+    public void pakaiHelm() {
+        System.out.println("Pengendara memakai helm SNI. Klik! Siap jalan.");
     }
 }
